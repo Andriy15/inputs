bc6395eff08009229978bfe847de80d71c4090ef
Andrii <andriychikulay@gmail.com>
2023-11-28 22:20:01 +0200

    formik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,86 @@
 'use client'
-import React, {useState} from 'react';
+import React from 'react';
 import { TextField } from '@mui/material';
 import {NumericFormat, PatternFormat} from "react-number-format";
 import MaskedInput from "react-text-mask";
+import {useFormik} from "formik";
 
 export default function Home() {
-  const [value, setValue] = useState("")
-  const [url, setUrl] = useState('')
-
+  const formik = useFormik({
+    initialValues: {
+      amount: '',
+      count: '',
+      phone: '',
+      domain: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  })
 
   const handleBlur = () => {
-    if (value && !/\.\d{2}$/.test(value)) {
-      setValue(value + ".00");
+    const { amount } = formik.values;
+    if (amount && !/\.\d{2}$/.test(amount)) {
+      formik.setFieldValue('amount', amount + '.00');
     }
   }
 
-  const handleFocus = () => {
-    let updatedUrl = url;
-    if (!url.includes('.com')) {
-      updatedUrl += '.com';
-    }
-    setUrl(updatedUrl)
-  }
-
-
-  const handleChange = (event: any) => {
-    setUrl(event.target.value)
-  }
-
-  const urlMask = [
+  const domainMask = [
     'https://',
     ...Array.from({ length: 30 }, () => /[a-z.]/),
   ]
 
   return (
      <div className="m-8">
-       <NumericFormat
-          value={value}
-          prefix="$"
-          thousandSeparator
-          customInput={TextField}
-          label="Amount"
-          onValueChange={({ value }) => setValue(value)}
-          onBlur={handleBlur}
-       />
+       <h1 className="text-4xl font-bold">Form</h1>
+       <form onSubmit={formik.handleSubmit}>
+         <NumericFormat
+            value={formik.values.amount}
+            prefix="$"
+            thousandSeparator
+            customInput={TextField}
+            label="Amount"
+            onValueChange={({ value }) => formik.handleChange('amount')(value)}
+            onBlur={handleBlur}
+         />
 
-       <NumericFormat
-          className='ml-6 mr-6'
-          customInput={TextField}
-          label="Count"
-          thousandSeparator
-       />
+         <NumericFormat
+            value={formik.values.count}
+            className="ml-6 mr-6"
+            customInput={TextField}
+            label="Count"
+            thousandSeparator
+            onValueChange={({ value }) => formik.handleChange('count')(value)}
+         />
 
-       <PatternFormat
-          format="+1 (###) #### ###"
-          customInput={TextField}
-          label="Phone"
-       />
+         <PatternFormat
+            value={formik.values.phone}
+            format="+1 (###) #### ###"
+            customInput={TextField}
+            label="Phone"
+            onValueChange={({ value }) => formik.handleChange('phone')(value)}
+         />
 
-       <MaskedInput
-          value={url}
-          onBlur={handleFocus}
-          className='ml-6'
-          onChange={handleChange}
-          mask={urlMask}
-          guide={false}
-          render={(ref, props) => (
-             <TextField
-                inputRef={ref}
-                {...props}
-                label="Domain"
-                variant="outlined"
-             />
-          )}
-       />
+         <TextField
+            className="ml-6 mr-6"
+            label="Domain"
+            value={formik.values.domain}
+            onChange={formik.handleChange('domain')}
+            InputProps={{
+              inputComponent: MaskedInput as any,
+              inputProps: {
+                mask: domainMask,
+              },
+            }}
+         />
+
+         <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+         >
+           Submit
+         </button>
+       </form>
      </div>
-  );
+  )
 }
-
